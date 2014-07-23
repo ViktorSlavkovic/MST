41c5236791b71a8c07d7e1e31b009d6dc05ca321
Viktor Slavkovic <viktor.slavkovic@gmail.com>
2014-07-23 17:18:53 +0200

    Commit OLD

--- MST/src/pack/viktor/pq/MinIdxPriorityQueue.java
+++ MST/src/pack/viktor/pq/MinIdxPriorityQueue.java
@@ -1,24 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package pack.viktor.pq;
 
 /**
+ * Interfejs klasa koje implementiraju strukturu indeksiranog
+ * reda sa prioritetom
  *
- * @author Viktor
+ * @author Viktor Slavkovic
  * @param <Key>
  */
+public interface MinIdxPriorityQueue< Key> {
 
-public interface MinIdxPriorityQueue < Key > {
     public void insert(int idx, Key val);
+
     public void deleteMin();
+
     public int getMinIdx();
+
     public Key getMin();
+
     public void decreaseKey(int idx, Key newVal);
+
     public boolean isEmpty();
+
     public Key getVal(int idx);
+
     public boolean exists(int idx);
 }
